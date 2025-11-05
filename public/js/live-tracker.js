d9f2c24e18f9e5c35fb2830a1f0a775d99869916
Radian-Motorsport <jdotsowerby@gmail.com>
2025-11-05 20:47:55 +0000

    ui changes

--- public/js/live-tracker.js
+++ public/js/live-tracker.js
@@ -471,10 +471,10 @@ class LiveStrategyTracker {
             
             // Initialize car position tracker after map loads
             this.carPositionTracker = new window.CarPositionTracker('track-map-container-live', {
-                carRadius: 12,
+                carRadius: 18, // was 12
                 carColor: '#06b6d4',  // Cyan
                 carStroke: '#0e7490',
-                carStrokeWidth: 3,
+                carStrokeWidth: 4,
                 trackLayerName: 'active',
                 useRacingLine: false  // Will be set to true if racing line data available
             });
